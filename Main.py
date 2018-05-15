f55e54fe179bb1028b32e28732443ca5a4ab8acd
joannis <joannisbs@gmail.com>
2018-05-14 21:04:23 -0300

    2v2.0.9
    Adcionado Tela 2 Mais relógios

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@
 from VariaveisGlobais import * 
 from Tkinter import *
 from TelaRelogio1 import *
+from TelaRelogio2 import *
 from TelaResumo import *
 import tkMessageBox as messagebox
 
@@ -43,7 +44,8 @@ def on_closing():
 class Iniciooo:
 
 	def __init__(self,root):
-
+		lable1 = Label(Telas.root, text = "LOAGING...")
+		lable1.grid(row=0,pady=5,padx=20)
 
 		Relo1 = Toplevel(master=None)
 		Relo1.geometry('1950x950')
@@ -56,6 +58,19 @@ class Iniciooo:
 		Relo1.title("Monitor Relogios 1")
 		Relo1.protocol("WM_DELETE_WINDOW",on_closing)
 		Telas.GUI_Tela1 = TelaRelogio1(Relo1)
+
+
+		Relo2 = Toplevel(master=None)
+		Relo2.geometry('1950x950')
+		Relo2.update()
+		Relo2.grid_rowconfigure(0,weight=1)
+		Relo2.grid_columnconfigure(0,weight=1)
+		Relo2.resizable(True,True)
+		Relo2.configure(background="black")		
+		#Relo2.geometry(Relo2.geometry())
+		Relo2.title("Monitor Relogios 2")
+		Relo2.protocol("WM_DELETE_WINDOW",on_closing)
+		Telas.GUI_Tela2 = TelaRelogio2(Relo2)
 
 		
 		
